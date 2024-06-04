3e5bdbae51e9ca58ca688fd563272b76bbcf300e
oscarpedrero <oscarpedrero@gmail.com>
2024-06-04 23:39:02 +0200

    Fixed Error in GithUb Actions

--- DB/Models/MethodsLog.cs
+++ DB/Models/MethodsLog.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BloodyWallet.DB.Models{
-    public class MethodsLog(){
+namespace BloodyWallet.DB.Models
+{
+    public class MethodsLog {
         public static readonly string FromCommandAdmin = "Command Admin";
         public static readonly string Fromtransfer = "Transfer";
     }
