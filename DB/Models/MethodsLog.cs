b33458554f31b0be9522f618570f13e93161c78f
oscarpedrero <oscarpedrero@gmail.com>
2024-06-04 23:37:22 +0200

    Solved errors for build github actions

--- DB/Models/MethodsLog.cs
+++ DB/Models/MethodsLog.cs
@@ -4,8 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BloodyWallet.DB.Models
-{
+namespace BloodyWallet.DB.Models{
     public class MethodsLog(){
         public static readonly string FromCommandAdmin = "Command Admin";
         public static readonly string Fromtransfer = "Transfer";
